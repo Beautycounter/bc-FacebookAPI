72d119a72a3cadaf328e3eba40e8a89958a7f73b
Brady Bastian <brady.bastian@beautycounter.com>
2020-07-14 09:38:13 -0700

    upd

--- Database/API/Insert Scripts/INSERT_API.CmdshellExecutions_PROD.sql
+++
@@ -1,8 +0,0 @@
-USE MailChimp
-
-TRUNCATE TABLE API.CmdshellExecutions 
-
-INSERT API.CmdshellExecutions
-
-SELECT 'GetCampaigns', '#insert .bet exe#'
-	, '"GetCampaigns" "SERVER=localhost;Database=MailChimp;UID=dataworker;PWD=dataworker1" "Campaigns.uspGetCampaignsParameters"'
